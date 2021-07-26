ed6d45d3da1b4d998881411edd6f61cc3f7d3301
Markus Schirp <mbj@schirp-dso.com>
2021-07-26 02:31:29 +0000

    Change to mhs

--- test/Test.hs
+++ test/Test.hs
@@ -1,21 +1,29 @@
 import MPrelude
+import Prelude (error)
 import Test.Tasty
 
 import qualified APK.StackDeploy          as StackDeploy
 import qualified Devtools
+import qualified MRIO.Amazonka            as AWS
 import qualified StackDeploy.InstanceSpec as InstanceSpec
 import qualified StackDeploy.Template     as Template
 
+data Env
+
+instance AWS.HasAWSEnv Env where
+  awsEnv _env = error "Intentionally undefined"
+
+instance AWS.HasResourceMap Env where
+  resourceMap _env = error "Intentionally undefined"
+
 main :: IO ()
-main =
-  defaultMain $
-    testGroup
-      "apk-build"
-        [ Devtools.testTree Devtools.defaultConfig
-        , template
-        ]
+main = do
+  devtools <- Devtools.testTree Devtools.defaultConfig
+    { Devtools.targets = [Devtools.Target "apk-build"] }
+
+  defaultMain $ testGroup "apk-build" [devtools, template]
 
 template :: TestTree
 template
   = Template.testTree
-  $ InstanceSpec.templateProvider StackDeploy.instanceSpecs
+  $ InstanceSpec.templateProvider (StackDeploy.instanceSpecs @Env)
